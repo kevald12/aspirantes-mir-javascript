42925c92118f1c1d30059916cf6ba4c20d772069
kevald12 <kealable@gmail.com>
2023-05-10 21:38:49 -0500

    solucion a los ejercicios de js

--- js-1/sum.js
+++ js-1/sum.js
@@ -1,12 +1,13 @@
 function sum (num){
-
+let sum = 0;
 for(let i= 1; i<=num; i++){
   sum = sum + i;
-  return  sum
+  
     }
-    
+    return  sum
 }
 console.log(sum(20))
-
-
+console.log(sum(4))
+console.log(sum(10))
+console.log(sum(15))
 
